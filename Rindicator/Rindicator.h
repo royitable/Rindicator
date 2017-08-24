b0df90a488f4450040c23da9f04eda4c966ae709
roy <royite.ren@gmail.com>
2017-08-24 23:43:47 +0800

    Initial Commit

---
+++ Rindicator/Rindicator.h
@@ -0,0 +1,19 @@
+//
+//  Rindicator.h
+//  Rindicator
+//
+//  Created by roy on 2017/8/24.
+//  Copyright © 2017年 Royite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Rindicator.
+FOUNDATION_EXPORT double RindicatorVersionNumber;
+
+//! Project version string for Rindicator.
+FOUNDATION_EXPORT const unsigned char RindicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rindicator/PublicHeader.h>
+
+
